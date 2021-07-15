3bffac17fecb847e6031cf17bd1f92c76ff3c8ab
Denis Shebalkov <87080104+ShebalkovD@users.noreply.github.com>
2021-07-15 17:41:00 +0300

    fixed bug with 1st task

--- main.js
+++ main.js
@@ -1,76 +1,71 @@
-const taskBtn = document.getElementById('btn');
-const inputText = document.getElementById('space__enter');
-const taskWrapper = document.getElementById('task__wrapper')
+const addTaskBtn = document.getElementById('btn')
+const inputTask = document.getElementById('space__enter')
+const todosWrapper = document.querySelector('.task__wrapper')
 
-let todosItems = [];
+ let tasks = []
 
-let tasks = [];
+ let todosItems = []
 
 !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'))
 
 function Task(description){
-    this.description = description,
+    this.description = description
     this.completed = false
-}
+ }
 
-const createTemplate = (task, index) => {
-    return `
-    <div class="todo-item">
-                <div class="description">${task.description}</div>
-                <div class="buttons">
-                    <input onclick = "completeTask(${index})" type="checkbox" class="btn-complete">
-                    <button onclick = "deleteTask(${index})"" class="btn-delete"><img src="/images/delete.svg" alt=""></button>
-                </div>
-            </div>
-    `
-   
-}
-
-const fillHtmlList = () => {
-    taskWrapper.innerHTML = "";
-    if(tasks.length > 0) {
-        tasks.forEach((item, index) =>{
-            taskWrapper.innerHTML += createTemplate(item, index);
-        });
-       todosItems = document.querySelectorAll('.todo-item') 
-       
-    }
-    inputText.value = "";
-}
-
-fillHtmlList()
 
 const updateLocal = () => {
     localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
+ addTaskBtn.onclick = () => {
+     if(inputTask.value != ""){tasks.push(new Task(inputTask.value))
+        updateLocal()
+        fillHtmlList()
+        inputTask.value = ""}
+ }
 
-taskBtn.onclick = () => {
-    tasks.push(new Task(inputText.value));
-    updateLocal();
-    fillHtmlList();
+ const createTemplate = (task,index) => {
+    return `
+    <div class="todo-item" ${task.completed ? 'active' : ''}>
+                <div class="description">${task.description}</div>
+                <div class="buttons">
+                    <input onclick = "completeTask(${index})" type="checkbox" class="btn-complete" ${task.completed ? 'checked' : ''}>
+                    <button onclick = "deleteTask(${index})"" class="btn-delete"><img src="/images/delete.svg" alt=""></button>
+                </div>
+            </div>
+    `
     
-}
+ }
 
-const completeTask = index => {
-    tasks[index].completed = !tasks[index].completed;
-    if(tasks[index].completed){
-        todosItems[index].classList.add('active');
-        document.querySelector('.btn-complete').innerHTML = '<img src="/images/checked.svg" alt="">'
-    }else{
-        todosItems[index].classList.remove('active');
-        document.querySelector('.btn-complete').innerHTML = ''
-    }
-    updateLocal();
-    
-}
+ const fillHtmlList = () => {
+     todosWrapper.innerHTML = ""
+     if(tasks.length > 0 ){
+         tasks.forEach((item, index) =>{
+            todosWrapper.innerHTML += createTemplate(item,index)
+         })
+         todosItems = document.querySelectorAll('.todo-item')
+     }  
+ }
+ updateLocal()
+ fillHtmlList()
 
-const deleteTask = index => {
-    todosItems[index].classList.add('deleted')
-    setTimeout(() => {
-        tasks.splice(index, 1);
-    updateLocal();
-    fillHtmlList();
-    }, 100 )
-}
+ const updatePage = () => {
+     updateLocal()
+     fillHtmlList()
+ }
 
+ const completeTask = index => {
+     tasks[index].completed = !tasks[index].completed
+     if(tasks[index].completed){
+        todosItems[index].classList.add('active')
+     }else{
+        todosItems[index].classList.remove('active')
+     }
+     updateLocal()
+ }
+
+ const deleteTask = index => {
+     tasks.splice(index, 1)
+     updatePage()
+ }
