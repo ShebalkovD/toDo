e51da86a29649c6e2a577a606363e9057f54af79
Denis Shebalkov <87080104+ShebalkovD@users.noreply.github.com>
2021-07-16 16:01:14 +0300

    added line between wrappers

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 const addTaskBtn = document.getElementById('btn')
 const inputTask = document.getElementById('space__enter')
 const todosWrapper = document.querySelector('.task__wrapper')
+const todosWrapperComplete = document.querySelector('.completed__wrapper')
+const lineWrap = document.getElementById('line')
 
  let tasks = []
 
  let todosItems = []
 
+ let completeTasks = []
+
 !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'))
+!localStorage.completeTasks ? completeTasks = [] : completeTasks = JSON.parse(localStorage.getItem('completeTasks'))
 
 function Task(description){
     this.description = description
     this.completed = false
  }
 
-
 const updateLocal = () => {
     localStorage.setItem('tasks', JSON.stringify(tasks))
+    localStorage.setItem('completeTaks', JSON.stringify(completeTasks))
 }
 
  addTaskBtn.onclick = () => {
@@ -27,7 +32,7 @@ const updateLocal = () => {
 
  const createTemplate = (task,index) => {
     return `
-    <div class="todo-item" ${task.completed ? 'active' : ''}>
+    <div class="todo-item" >
                 <div class="description">${task.description}</div>
                 <div class="buttons">
                     <input onclick = "completeTask(${index})" type="checkbox" class="btn-complete" ${task.completed ? 'checked' : ''}>
@@ -40,11 +45,23 @@ const updateLocal = () => {
 
  const fillHtmlList = () => {
      todosWrapper.innerHTML = ""
+     todosWrapperComplete.innerHTML = ""
      if(tasks.length > 0 ){
          tasks.forEach((item, index) =>{
-            todosWrapper.innerHTML += createTemplate(item,index)
+             tasks[index].completed ? todosWrapperComplete.innerHTML += createTemplate(item,index):todosWrapper.innerHTML += createTemplate(item,index)
          })
          todosItems = document.querySelectorAll('.todo-item')
+         if(todosWrapperComplete.innerHTML != "" && todosWrapper.innerHTML != ""){
+             todosWrapperComplete.innerHTML += `
+             <div class="completed__wrapper">
+            <div class="line-arrow"id="line">
+                <div class="line" ></div>
+            </div>
+        </div>`
+             lineWrap.classList.add('flexx')
+         }else{
+            lineWrap.classList.remove('flexx') 
+         }
      }  
  }
  updateLocal()
@@ -53,16 +70,20 @@ const updateLocal = () => {
  const updatePage = () => {
      updateLocal()
      fillHtmlList()
- }
 
- const completeTask = index => {
+    }
+
+   
+    
+ const completeTask = (index) => {
      tasks[index].completed = !tasks[index].completed
      if(tasks[index].completed){
         todosItems[index].classList.add('active')
+        
      }else{
         todosItems[index].classList.remove('active')
      }
-     updateLocal()
+     updatePage()
  }
 
  const deleteTask = index => {
